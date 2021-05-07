e7a5ab79ef0a7636dfd997467866feb18603732b
matheus <matheus.xmaz10@gmail.com>
2021-05-07 16:52:08 -0300

    correct indentation

--- plugins/tvControls.js
+++ plugins/tvControls.js
@@ -1,61 +1,58 @@
-let televisao = document.querySelector('#televisao')
-let videoPlayer = document.querySelector('video[name=videoPlayer]')
+let televisao = document.querySelector("#televisao");
+let videoPlayer = document.querySelector("video[name=videoPlayer]");
 
 function TVcontrols() {
-    let controls = document.querySelector("div.controls .btn-controls");
-    
-    controls.style.visibility = "hidden";
-    
-    setTimeout(() => {
-        controls.style.visibility = "visible";
-    }, 2000);
+  let controls = document.querySelector("div.controls .btn-controls");
 
-    controls.onclick = function () {
-        switch (controls.dataset.target) {
-            case 'pausar':
-                videoPlayer.pause()
-                controls.innerText = "play_arrow"
-                controls.dataset.target = "play"
-                break;
+  controls.style.visibility = "hidden";
 
-            case 'play':
-            default:
-                videoPlayer.play()
-                controls.innerText = "pause"
-                controls.dataset.target = "pausar"
-                break;
-        }
+  setTimeout(() => {
+    controls.style.visibility = "visible";
+  }, 2000);
+
+  controls.onclick = function () {
+    switch (controls.dataset.target) {
+      case "pausar":
+        videoPlayer.pause();
+        controls.innerText = "play_arrow";
+        controls.dataset.target = "play";
+        break;
+
+      case "play":
+      default:
+        videoPlayer.play();
+        controls.innerText = "pause";
+        controls.dataset.target = "pausar";
+        break;
     }
+  };
 
-    let fixed = document.querySelector("div.controls .btn-fixed");
-    fixed.style.visibility = "hidden";
-    setTimeout(() => {
-        fixed.style.visibility = "visible";
-    }, 2000);
+  let fixed = document.querySelector("div.controls .btn-fixed");
+  fixed.style.visibility = "hidden";
+  setTimeout(() => {
+    fixed.style.visibility = "visible";
+  }, 2000);
 
-    fixed.onclick = function () {
-        videoPlayer.requestPictureInPicture();
-    }
+  fixed.onclick = function () {
+    videoPlayer.requestPictureInPicture();
+  };
 
-    let fullscreen = document.querySelector(".btn-fullscreen")
-    fullscreen.onclick = function(){
-        videoPlayer.requestFullscreen()
-    }
+  let fullscreen = document.querySelector(".btn-fullscreen");
+  fullscreen.onclick = function () {
+    videoPlayer.requestFullscreen();
+  };
 }
 
-TVcontrols()
+TVcontrols();
 
 function videoPlayerProperties() {
-    videoPlayer.autoplay = true;
-    videoPlayer.controls = false;
-    videoPlayer.style = "width:800px; height:450px;"
-    videoPlayer.id = "example-video"
-    videoPlayer.classList.add("video-js", "vjs-default-skin")
-    videoPlayer.textTracks.mode = 'disabled';
-    
-    console.log(videoPlayer.videoTracks)
+  videoPlayer.autoplay = true;
+  videoPlayer.controls = false;
+  videoPlayer.style = "width:800px; height:450px;";
+  videoPlayer.id = "example-video";
+  videoPlayer.classList.add("video-js", "vjs-default-skin");
+  videoPlayer.textTracks.mode = "disabled";
+  console.log(videoPlayer.videoTracks);
 }
 
-videoPlayerProperties()
-
-
+videoPlayerProperties();
